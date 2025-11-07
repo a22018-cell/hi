a2527186a4cd9d98b92be9a1ad1f5df4d0c934ff
a22018-cell <a22018@farmroad.edu.hk>
2025-11-07 12:49:10 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,15 @@
+if (input.buttonIsPressed(Button.A)) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+}
+if (input.buttonIsPressed(Button.B)) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+}
+if (input.buttonIsPressed(Button.AB)) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+}
